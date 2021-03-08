74e0eaa3389807c30a3bf6d09e9443140dffdb38
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2021-03-08 22:05:09 +0200

    fixed migrations for development

--- backend/src/data/db/knexfile.ts
+++ backend/src/data/db/knexfile.ts
@@ -3,6 +3,13 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://postgres:11037853@127.0.0.1/cte',
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 
   staging: {
