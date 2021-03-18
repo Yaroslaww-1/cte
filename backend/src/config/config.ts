fe9f29cd2766e6ccd8f4c9ad23a5fa2bea2aab82
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2021-03-18 20:28:08 +0200

    config was configured

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,9 +2,11 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { IBackendApplicationConfig } from './backend-application.config';
 import { IDatabaseConfig } from './database.config';
+import { IMailerConfig } from './mailer.config';
 
 export const BACKEND_APPLICATION_CONFIG = 'BACKEND_APPLICATION';
 export const DATABASE_CONFIG = 'DATABASE';
+export const MAILER_CONFIG = 'MAILER';
 
 export const configFactory = () => ({
 	[BACKEND_APPLICATION_CONFIG]: {
@@ -14,4 +16,10 @@ export const configFactory = () => ({
 	[DATABASE_CONFIG]: {
 		CONNECTION_URL: process.env.DATABASE_URL,
 	} as IDatabaseConfig,
+	[MAILER_CONFIG]: {
+		MAILER_HOST: process.env.MAILER_HOST,
+    	MAILER_PORT: parseInt(process.env.MAILER_PORT || '', 10),
+    	EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
+    	EMAIL_PASS: process.env.EMAIL_PASS,
+	} as IMailerConfig,
 });
