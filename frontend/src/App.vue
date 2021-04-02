47f0ce9fe5f8866f245f123143a11926585cee44
fireman9 <nazariystr@gmail.com>
2021-04-02 14:10:59 +0300

    deleted router nav for testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,17 @@
 <template>
-	<div id="nav">
+	<!-- <div id="nav">
 		<router-link to="/">Home</router-link>
 		<router-link to="/about">About</router-link>
-	</div>
+		<router-link to="/editor">Editor</router-link>
+	</div> -->
 	<router-view />
 </template>
 
 <style lang="scss">
+body {
+	padding: 0;
+	margin: 0;
+}
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
