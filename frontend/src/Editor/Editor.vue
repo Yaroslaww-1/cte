f687c146cff419d66ff5ea472a3f85dd4e314b58
fireman9 <nazariystr@gmail.com>
2021-04-02 14:14:26 +0300

    inserted Button.vue and new modules into editor

--- frontend/src/Editor/Editor.vue
+++ frontend/src/Editor/Editor.vue
@@ -1,12 +1,26 @@
 <template>
-	<div class="buttons"></div>
+	<div class="buttons">
+		<Button
+			v-for="(module, i) in modules"
+			:key="module.title + i"
+			:title="module.description"
+			:icon="module.icon"
+			:action="module.action"
+		/>
+	</div>
 	<div id="editor" contenteditable spellcheck="false">Enter text here...</div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Button from '@src/Editor/Button.vue';
+import bold from './modules/bold';
+import italic from './modules/italic';
 
 @Options({
+	components: {
+		Button,
+	},
 	methods: {
 		execute(command: string, arg?: string): void {
 			const editor = document.getElementById('editor');
@@ -15,7 +29,9 @@ import { Options, Vue } from 'vue-class-component';
 		},
 	},
 })
-export default class Editor extends Vue {}
+export default class Editor extends Vue {
+	modules = [bold, italic];
+}
 </script>
 
 <style lang="scss">
