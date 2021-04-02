4363b6d76ba488263f5a300da977916f9699d3b1
fireman9 <nazariystr@gmail.com>
2021-04-02 14:09:30 +0300

    Added editor component(contenteditable div)

---
+++ frontend/src/Editor/Editor.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="buttons"></div>
+	<div id="editor" contenteditable spellcheck="false">Enter text here...</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+	methods: {
+		execute(command: string, arg?: string): void {
+			const editor = document.getElementById('editor');
+			if (editor !== null) editor.focus();
+			document.execCommand(command, false, arg || '');
+		},
+	},
+})
+export default class Editor extends Vue {}
+</script>
+
+<style lang="scss">
+#editor {
+	background-color: #1e2126;
+	color: #abb2bf;
+	min-height: 90vh;
+	padding: 10px;
+}
+.buttons {
+	display: flex;
+	border: 1px solid grey;
+}
+</style>
