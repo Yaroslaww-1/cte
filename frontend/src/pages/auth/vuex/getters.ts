1d21f738c9090808afc5d953fb7e53c3ce63604a
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-04-05 11:58:46 +0300

    frontend: add vuex-module-decorators

--- frontend/src/pages/auth/vuex/getters.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-
-export default {
-  userId(state: any): any {
-    return state.userName;
-  },
-};
