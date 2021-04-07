92d21b5fa13068abd7aeeef54910b251da11d3a2
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-04-08 00:23:01 +0300

    backend update auth migrations

--- backend/src/data/migrations/20210321170810_add_auth_info_to_user.ts
+++ backend/src/data/migrations/20210321170810_add_auth_info_to_user.ts
@@ -2,14 +2,14 @@ import Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable('users', table => {
-    table.text('passwordHash').notNullable();
-
-    //emailConfirmToken
+    table.text('password_hash').notNullable();
+    table.text('email_confirm_token').nullable();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.alterTable('users', table => {
-    table.dropColumn('passwordHash');
+    table.dropColumn('password_hash');
+    table.dropColumn('email_confirm_token');
   });
 }
