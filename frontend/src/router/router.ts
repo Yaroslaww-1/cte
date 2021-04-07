b9181e961768bf63d23c9b5827fa11e5cedee035
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-04-08 00:21:32 +0300

    frontend: fix import errors

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-import LoginPage from '@pages/auth/login-page.vue';
+import LoginPage from '@pages/auth/login.page.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
