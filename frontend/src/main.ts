3f1bdd4487698539846fc623fcbdaedbce7dd2a1
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-04-08 00:20:42 +0300

    frontend: move store in the top of vue app initialization

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
+import store from './vuex/store';
 import App from './App.vue';
 import router from './router/router';
-import store from './vuex/store';
 
 const app = createApp(App);
 
-app.use(router);
 app.use(store);
+app.use(router);
 
 app.mount('#app');
