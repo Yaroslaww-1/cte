c60c1f975c4dae299a2f27e8712a37c4bb3b31a5
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-04-10 12:32:12 +0300

    backend: fix imports

--- backend/src/core/lib/jwt.lib.ts
+++ backend/src/core/lib/jwt.lib.ts
@@ -1,11 +1,11 @@
-import jwt, { SignOptions } from 'jsonwebtoken';
+import { verify, sign, SignOptions } from 'jsonwebtoken';
 import { TokenExpiredException } from '../exceptions/jwt-token/token-expired.exception';
 import { TokenNotSignedException } from '../exceptions/jwt-token/token-not-signed.exception copy';
 import { TokenNotVerifiedException } from '../exceptions/jwt-token/token-not-verified.exception';
 
 const jwtVerify = (SECRET: string) => <T>(token: string): Promise<T> => {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, SECRET, (error, decoded) => {
+    verify(token, SECRET, (error, decoded) => {
       if (error) {
         if (error.name === 'TokenExpiredError') {
           return reject(new TokenExpiredException());
@@ -22,7 +22,7 @@ type JwtSignOptions = Omit<SignOptions, 'algorithm'> & { algorithm: string };
 
 const jwtSign = (SECRET: string) => (payload: JwtPayloadType, options: JwtSignOptions): Promise<string> => {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, SECRET, options as SignOptions, (error, token) => {
+    sign(payload, SECRET, options as SignOptions, (error, token) => {
       if (error) {
         return reject(new TokenNotSignedException(error.message));
       } else {
