2240d748bd28b140afa0f0b34ff6679a81f8e91b
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-04-28 21:27:56 +0300

    frontend: fixed svg loader

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,6 +5,17 @@ const path = require('path');
 module.exports = {
 	chainWebpack: config => {
 		config.resolve.alias.set('@src', path.resolve(__dirname, 'src'));
+
+		const svgRule = config.module.rule('svg');
+
+		svgRule.uses.clear();
+
+		svgRule
+			.use('vue-loader')
+			.loader('vue-loader-v16') // or `vue-loader-v16` if you are using a preview support of Vue 3 in Vue CLI
+			.end()
+			.use('vue-svg-loader')
+			.loader('vue-svg-loader');
 	},
 	lintOnSave: process.env.NODE_ENV !== 'production',
 };
