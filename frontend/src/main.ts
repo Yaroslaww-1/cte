5b31dc5da7a318514eaf5fbf3d18a1e3b1fd0382
Artem Karimov <59018831+tyomakarimov@users.noreply.github.com>
2021-04-29 18:45:26 +0300

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './vuex/store';
-import PriveVue from 'primevue/config';
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
-app.use(PriveVue);
+app.use(PrimeVue);
 
 app.mount('#app');
