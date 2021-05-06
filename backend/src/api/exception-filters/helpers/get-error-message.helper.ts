cabbe156c571faa784be099e8fa104cde4d5ac3e
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-05-06 08:35:36 +0300

    backend: simplified getErrorMessage

--- backend/src/api/exception-filters/helpers/get-error-message.helper.ts
+++ backend/src/api/exception-filters/helpers/get-error-message.helper.ts
@@ -2,13 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpException } from '@nestjs/common';
 
-import { ValidationException } from '@src/core/exceptions/validation.exception';
-
 const getErrorMessage = (exception: HttpException): string => {
-  if (exception instanceof ValidationException) {
-    return exception.message;
-  }
-
   return exception.message;
 };
 
