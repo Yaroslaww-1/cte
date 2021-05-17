1bed123efeb30ae8896b7f7fdbf90a71463e1ccc
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-05-17 19:53:56 +0300

    backend: updated update document flow to notify user in the same room

--- cte-shared/ws/emits-payload/document/index.ts
+++ cte-shared/ws/emits-payload/document/index.ts
@@ -4,13 +4,13 @@ import {
 } from './start-editing-document.client-emit-payload';
 
 import {
-  UPDATE_DOCUMENT_CLIENT_EMIT_EVENT,
-  UpdateDocumentClientEmitPayload,
-} from './update-document.client-emit-payload copy';
+  UPDATE_DOCUMENT_CLIENT_SERVER_EMIT_EVENT,
+  UpdateDocumentClientServerEmitPayload,
+} from './update-document.client-server-emit-payload';
 
 export {
   START_EDITING_DOCUMENT_CLIENT_EMIT_EVENT,
   StartEditingDocumentClientEmitPayload,
-  UPDATE_DOCUMENT_CLIENT_EMIT_EVENT,
-  UpdateDocumentClientEmitPayload,
+  UPDATE_DOCUMENT_CLIENT_SERVER_EMIT_EVENT,
+  UpdateDocumentClientServerEmitPayload,
 };
