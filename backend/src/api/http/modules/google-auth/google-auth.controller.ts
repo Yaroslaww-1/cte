87d116f9545d09eec3642ddd2182c2db56af1e1b
fireman9 <nazariystr@gmail.com>
2021-05-18 17:40:58 +0300

    added google-auth login or register then login

--- backend/src/api/http/modules/google-auth/google-auth.controller.ts
+++ backend/src/api/http/modules/google-auth/google-auth.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
-import { UserDto } from '@shared/dto';
+import { LoginSuccessResponse } from '@shared/request-response';
+import { IBackendApplicationConfig } from '@src/config/backend-application.config';
+import { BACKEND_APPLICATION_CONFIG } from '@src/config/config';
 import { GoogleCreateOrLoginUserUsecase } from '@src/core/services/google-auth/google-create-or-login-user.usecase';
+import { Response } from 'express';
 
 @Controller('google-auth')
 export class GoogleAuthController {
-  constructor(private readonly googleCreateUserUsecase: GoogleCreateOrLoginUserUsecase) {}
+  constructor(
+    private configService: ConfigService,
+    private readonly googleCreateUserUsecase: GoogleCreateOrLoginUserUsecase,
+  ) {}
 
   @Get()
   @UseGuards(AuthGuard('google'))
@@ -14,12 +21,28 @@ export class GoogleAuthController {
 
   @Get('callback')
   @UseGuards(AuthGuard('google'))
-  /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  googleAuthRedirect(@Req() req: any): Promise<UserDto> {
-    return this.googleCreateUserUsecase.execute(req, {
+  async googleAuthRedirect(
+    /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    @Req() req: any,
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<LoginSuccessResponse> {
+    const loginSuccessResponse = await this.googleCreateUserUsecase.execute(req, {
       name: req.user.firstName + ' ' + req.user.lastName,
       email: req.user.email,
+    });
+
+    const domain = this.configService.get<IBackendApplicationConfig>(BACKEND_APPLICATION_CONFIG)?.BACKEND_COOKIE_DOMAIN;
+    response.cookie('refreshTokenId', loginSuccessResponse.refreshTokenId, {
+      domain,
+      path: '/api/auth',
+      maxAge: loginSuccessResponse.refTokenExpiresInSeconds,
+      secure: false,
+    });
+
+    return await LoginSuccessResponse.new(LoginSuccessResponse, {
+      accessToken: loginSuccessResponse.accessToken,
+      refreshTokenId: loginSuccessResponse.refreshTokenId,
     });
   }
 }
--- backend/src/core/services/google-auth/google-create-or-login-user.usecase.ts
+++ backend/src/core/services/google-auth/google-create-or-login-user.usecase.ts
@@ -1,27 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { UserDto } from '@shared/dto';
 import { UserMapper } from '../user/user.mapper';
 import { UserDao } from '@src/data/dao/user/user.dao';
 import { GoogleUserRequest } from '@shared/request-response/user/google-create-user.request';
 import { UserEntity } from '../user/entities/user.entity';
 import { BaseEntity } from '@src/core/abstraction/base-entity';
+import { RefreshSessionService } from '../auth/services/refresh-session.service';
+import { AccessTokenService } from '../auth/services/access-token.service';
+import { REFRESH_TOKEN_LIFETIME_IN_SECONDS } from '../auth/constants';
+import { NotFoundException } from '@src/core/exceptions/not-found.exception';
+import { RefreshSessionEntity } from '../auth/entities/refresh-session.entity';
+import { LoginSuccessDto } from '../auth/dto/login-success.dto';
 
 @Injectable()
 export class GoogleCreateOrLoginUserUsecase {
-  constructor(private readonly userDao: UserDao, private readonly userMapper: UserMapper) {}
+  constructor(
+    private readonly userDao: UserDao,
+    private readonly userMapper: UserMapper,
+    private readonly refreshSessionService: RefreshSessionService,
+    private readonly accessTokenService: AccessTokenService,
+  ) {}
   /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async execute(request: any, googleUserRequest: GoogleUserRequest): Promise<UserDto> {
-    const userModel = await this.userDao.findOne({ email: googleUserRequest.email });
+  async execute(request: any, googleUserRequest: GoogleUserRequest): Promise<LoginSuccessDto> {
+    let userModel = await this.userDao.findOne({ email: googleUserRequest.email });
     if (!userModel) {
       const user = await BaseEntity.new(UserEntity, {
         name: googleUserRequest.name,
         email: googleUserRequest.email,
       });
       await this.userDao.createOne(user);
-      return await this.userMapper.mapToDto(user);
     }
-    // login procedure here...
-    return await this.userMapper.mapToDto(userModel);
+
+    const refTokenExpiresInMilliseconds = new Date().getTime() + REFRESH_TOKEN_LIFETIME_IN_SECONDS * 1000;
+    const refTokenExpiresInSeconds = refTokenExpiresInMilliseconds / 1000;
+
+    userModel = await this.userDao.findOne({ email: googleUserRequest.email });
+    if (!userModel) {
+      throw new NotFoundException('user');
+    }
+    const user = await UserEntity.new(UserEntity, userModel);
+
+    const newRefreshSession = await RefreshSessionEntity.newWithoutRefreshTokenId({
+      userId: user.id,
+      ip: request.ip,
+      fingerprint: 'google-fingerprint',
+      expiresInMs: refTokenExpiresInMilliseconds,
+    });
+    await this.refreshSessionService.createRefreshSession(newRefreshSession);
+
+    const accessToken = await this.accessTokenService.makeAccessToken(user);
+    const refreshTokenId = newRefreshSession.refreshTokenId;
+
+    return await LoginSuccessDto.new(LoginSuccessDto, {
+      accessToken,
+      refreshTokenId,
+      refTokenExpiresInSeconds,
+    });
   }
 }
