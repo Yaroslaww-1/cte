bdb2a0c8670c4cf134af2f6a7918e674af9191fd
fireman9 <nazariystr@gmail.com>
2021-05-18 17:39:12 +0300

    imported files for login via google

--- backend/src/api/http/modules/google-auth/google-auth.module.ts
+++ backend/src/api/http/modules/google-auth/google-auth.module.ts
@@ -5,14 +5,20 @@ import { ConfigModule } from '@nestjs/config';
 import { GoogleCreateOrLoginUserUsecase } from '@src/core/services/google-auth/google-create-or-login-user.usecase';
 import { UserDao } from '@src/data/dao/user/user.dao';
 import { UserMapper } from '@src/core/services/user/user.mapper';
+import { RefreshSessionService } from '@src/core/services/auth/services/refresh-session.service';
+import { AccessTokenService } from '@src/core/services/auth/services/access-token.service';
+import { RefreshSessionMapper } from '@src/core/services/auth/refresh-session.mapper';
+import { RefreshSessionDao } from '@src/data/dao/refresh-session/refresh-session.dao';
+import { SharedServicesModule } from '@src/api/shared-services.module';
 
-const mappers = [UserMapper];
-const daos = [UserDao];
+const mappers = [UserMapper, RefreshSessionMapper];
+const daos = [UserDao, RefreshSessionDao];
+const services = [RefreshSessionService, AccessTokenService];
 const usecases = [GoogleCreateOrLoginUserUsecase];
 
 @Module({
-  imports: [ConfigModule],
+  imports: [SharedServicesModule, ConfigModule],
   controllers: [GoogleAuthController],
-  providers: [...mappers, ...daos, ...usecases, GoogleStrategy],
+  providers: [...mappers, ...daos, ...usecases, ...services, GoogleStrategy],
 })
 export class GoogleAuthModule {}
